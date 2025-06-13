6e2e56ed16c32c52f4a4229a2b9256cd83c1ce18
julien97300 <applewhitejulien@gmail.com>
2025-06-13 07:50:12 -0300

    Add chemical_lab_erp_complete_websocket/chemical_lab_erp/frontend/chemical_lab_erp_frontend/src/components/sections/index.js 1749811811748

---
+++ chemical_lab_erp_complete_websocket/chemical_lab_erp/frontend/chemical_lab_erp_frontend/src/components/sections/index.js
@@ -0,0 +1,8 @@
+// Créer les autres composants de section rapidement
+export { default as Suppliers } from './Suppliers'
+export { default as Stock } from './Stock'
+export { default as Reports } from './Reports'
+export { default as Settings } from './Settings'
+export { default as Forum } from './Forum'
+export { default as Chat } from './Chat'
+
